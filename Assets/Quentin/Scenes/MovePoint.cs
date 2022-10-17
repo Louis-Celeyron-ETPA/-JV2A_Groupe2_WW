307f1684da6ad603e649f15311430c1253a0cd88
RENNES\q.izadpanah <q.izadpanah@rns.ecolescreatives.com>
2022-10-17 16:32:55 +0200

    Minijeu1_Quentin

---
+++ Assets/Quentin/Scenes/MovePoint.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePoint : MonoBehaviour
+
+{
+    // Start is called before the first frame update
+    public Transform MyTransform;
+    public float speed = 0.9f;
+
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Z))
+        {
+            MyTransform.position += MyTransform.up * speed / 2;
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            MyTransform.position -= MyTransform.right * speed / 2;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            MyTransform.position -= MyTransform.up * speed / 2;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            MyTransform.position += MyTransform.right * speed / 2;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            speed = speed / 4;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            speed = speed * 4;
+        }
+
+
+
+    }
+}
