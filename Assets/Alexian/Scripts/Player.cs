392fb3c1ce5621f4ff9659af71462c56f18be0ff
a.raguin <a.raguin@rns.ecolescreatives.com>
2022-10-20 09:44:13 +0200

    Repush Scene et mini-jeu 1

---
+++ Assets/Alexian/Scripts/Player.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Alexian
+{
+    public class Player : MonoBehaviour
+    {
+        public Transform player;
+        public Transform ennemy;
+        public bool canMove;
+        public TEMPOTOUCHES difficulty;
+        public Ending end;
+        public Music audios;
+
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (player.position.y >= 15 || ennemy.position.y >= 15)
+            {
+                canMove = false;
+                end.EndGame();
+                audios.audios.Stop();
+            }
+        }
+
+        public void Move()
+        {
+            if (canMove == false)
+            {
+                return;
+            }
+            player.position += Vector3.up * (0.5f * difficulty.difficulty);
+        }
+    }
+}
---
+++ Assets/Alexian/Scripts/TEMPOTOUCHES.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Alexian
+{
+    public class TEMPOTOUCHES : MonoBehaviour
+    {
+        public Player player;
+        public bool actionButton;
+        public float difficulty = 1f;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space) == true)
+            {
+                player.Move();
+            }
+        }
+    }
+}
