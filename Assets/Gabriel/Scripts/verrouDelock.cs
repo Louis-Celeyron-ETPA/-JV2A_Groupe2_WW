78f3731123dae10df2b357cafe1431d56330fc5b
RENNES\g.deleporte <gabysouri@gmail.com>
2022-10-20 10:44:02 +0200

    C'est bon gros y a les textures

--- Assets/Gabriel/Scripts/verrouDelock.cs
+++ Assets/Gabriel/Scripts/verrouDelock.cs
@@ -9,13 +9,24 @@ namespace Gabriel
 
         private List<int> sensVerrou = new List<int>();
 
+        public GameObject myVerrouDroite;
+        public GameObject myVerrouGauche;
         private int myVerrou = 0;
         // Start is called before the first frame update
         void Start()
         {
             for (int i = 0; i < 4; i++)
             {
-                sensVerrou.Add(Random.Range(1, 3));
+                int verrouRandom = Random.Range(1, 3);
+                sensVerrou.Add(verrouRandom);
+                if (verrouRandom == 1)
+                {
+                    Instantiate(myVerrouDroite, new Vector3(2, 2 - i, -13), Quaternion.identity);
+                }
+                else if (verrouRandom == 2)
+                {
+                    Instantiate(myVerrouGauche, new Vector3(2, 2 - i, -13), Quaternion.identity);
+                }
             }
         }
 
