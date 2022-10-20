2bd7f2971dfc1af859b14d482ec32c1f4eb3b7e9
RENNES\v.fabre <v.fabre@rns.ecolescreatives.com>
2022-10-20 11:00:59 +0200

    Camera Bound (le viseur boucle quand il sort de l'ecran)

--- Assets/Victor/Scripts/fonctionMove.cs
+++ Assets/Victor/Scripts/fonctionMove.cs
@@ -4,20 +4,47 @@ using UnityEngine;
 namespace victor {
     public class fonctionMove : MonoBehaviour
     {
-        public  Transform subject ;
-
+        public Transform subject ;
+        public Camera mainCamera;
+            
         public  float speed = 1f;
+
 
         // Start is called before the first frame update
         void Start()
         {
-
+            mainCamera = Camera.main;
         }
 
         // Update is called once per frame
         void Update()
         {
 
+            if(mainCamera.WorldToViewportPoint(transform.position).x> 1)
+            {
+                var tempVectorZero = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, mainCamera.farClipPlane));
+
+                transform.position = new Vector3(tempVectorZero.x, transform.position.y, transform.position.z);
+            }
+            if (mainCamera.WorldToViewportPoint(transform.position).x < 0)
+            {
+                var tempVectorOne = mainCamera.ViewportToWorldPoint(new Vector3(1, 1, mainCamera.farClipPlane));
+
+                transform.position = new Vector3(tempVectorOne.x, transform.position.y, transform.position.z);
+            }
+
+            if (mainCamera.WorldToViewportPoint(transform.position).y > 1)
+            {
+                var tempVectorZero = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, mainCamera.farClipPlane));
+
+                transform.position = new Vector3(transform.position.x, tempVectorZero.y, transform.position.z);
+            }
+            if (mainCamera.WorldToViewportPoint(transform.position).y < 0)
+            {
+                var tempVectorOne = mainCamera.ViewportToWorldPoint(new Vector3(1, 1, mainCamera.farClipPlane));
+
+                transform.position = new Vector3(transform.position.x, tempVectorOne.y, transform.position.z);
+            }
         }
 
         public void MoveUp()
