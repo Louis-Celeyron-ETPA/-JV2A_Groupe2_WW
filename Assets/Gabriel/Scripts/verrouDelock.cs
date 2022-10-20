640d1106a234229de1a0adb2f707d09a043d16e5
RENNES\g.deleporte <gabysouri@gmail.com>
2022-10-20 10:57:27 +0200

    nouvelles directions

--- Assets/Gabriel/Scripts/verrouDelock.cs
+++ Assets/Gabriel/Scripts/verrouDelock.cs
@@ -8,24 +8,40 @@ namespace Gabriel
     {
 
         private List<int> sensVerrou = new List<int>();
+        public int nombreVerrou;
 
+        public GameObject myVerrouBouton;
         public GameObject myVerrouDroite;
         public GameObject myVerrouGauche;
+        public GameObject myVerrouHaut;
+        public GameObject myVerrouBas;
         private int myVerrou = 0;
         // Start is called before the first frame update
         void Start()
         {
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < nombreVerrou; i++)
             {
-                int verrouRandom = Random.Range(1, 3);
+                int verrouRandom = Random.Range(0, 5);
                 sensVerrou.Add(verrouRandom);
-                if (verrouRandom == 1)
+                if (verrouRandom == 0)
+                {
+                    Instantiate(myVerrouBouton, new Vector3(2, 2 - i, -13), Quaternion.identity);
+                }
+                else if (verrouRandom == 1)
                 {
                     Instantiate(myVerrouDroite, new Vector3(2, 2 - i, -13), Quaternion.identity);
                 }
                 else if (verrouRandom == 2)
                 {
                     Instantiate(myVerrouGauche, new Vector3(2, 2 - i, -13), Quaternion.identity);
+                }
+                else if (verrouRandom == 3)
+                {
+                    Instantiate(myVerrouHaut, new Vector3(2, 2 - i, -13), Quaternion.identity);
+                }
+                else if (verrouRandom == 4)
+                {
+                    Instantiate(myVerrouBas, new Vector3(2, 2 - i, -13), Quaternion.identity);
                 }
             }
         }
@@ -35,6 +51,13 @@ namespace Gabriel
         {   
             if (myVerrou < sensVerrou.Count)
             {
+
+                if ((Input.GetKeyDown(KeyCode.Space)) && (sensVerrou[myVerrou] == 0))
+                {
+                    transform.position += Vector3.down;
+                    myVerrou++;
+                }
+
                 if ((Input.GetKeyDown(KeyCode.RightArrow)) && (sensVerrou[myVerrou] == 1))
                 {
                     transform.position += Vector3.down;
@@ -42,6 +65,18 @@ namespace Gabriel
                 }
 
                 if ((Input.GetKeyDown(KeyCode.LeftArrow)) && (sensVerrou[myVerrou] == 2))
+                {
+                    transform.position += Vector3.down;
+                    myVerrou++;
+                }
+
+                if ((Input.GetKeyDown(KeyCode.UpArrow)) && (sensVerrou[myVerrou] == 3))
+                {
+                    transform.position += Vector3.down;
+                    myVerrou++;
+                }
+
+                if ((Input.GetKeyDown(KeyCode.DownArrow)) && (sensVerrou[myVerrou] == 4))
                 {
                     transform.position += Vector3.down;
                     myVerrou++;
