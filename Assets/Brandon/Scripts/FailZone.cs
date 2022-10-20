0021325b27e9919dba566d390fa5b4897c736ba3
BGouet <b.gouet@rns.ecolescreatives.com>
2022-10-20 11:02:43 +0200

    Input+FailZone

--- Assets/Brandon/Scripts/FailZone.cs
+++ Assets/Brandon/Scripts/FailZone.cs
@@ -4,31 +4,17 @@ using UnityEngine;
 
 namespace Brandon
 {
-    public class Trigger : MonoBehaviour
+    public class FailZone : MonoBehaviour
     {
-        public int direction;
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
-
         private void OnTriggerEnter2D(Collider2D collision)
         {
             Voleur voleurTriggered = collision.GetComponent<Voleur>();
             if (voleurTriggered != null)
             {
-                voleurTriggered.direction = direction;
+                voleurTriggered.gameOver = true;
             }
 
         }
     }
 }
-
 
