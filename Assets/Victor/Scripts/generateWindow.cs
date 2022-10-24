42fb4ab1e24f985ccc4ec7fa4b281c5905704b9f
RENNES\v.fabre <victor.fabre@lahiette.com>
2022-10-24 14:29:17 +0200

    test appartition fentre

--- Assets/Victor/Scripts/generateWindow.cs
+++ Assets/Victor/Scripts/generateWindow.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace victor
 {
-    public class generateWindow : MonoBehaviour
-    {
+    
+public class generateWindow : MonoBehaviour {
+
+        public GameObject maFenetre;
+        public List<int> typeWindow = new List<int>();
+        public int nombrewnWindow;
+
+
         // Start is called before the first frame update
         void Start()
         {
+        for (int i = 0; i < nombrewnWindow; i++)
+        {
+            int fenetreRandom = Random.Range(0, 1);
+                typeWindow.Add(fenetreRandom);
 
+            Instantiate(maFenetre, new Vector3(2, 2 - i, -13), Quaternion.identity);
         }
+    }
 
         // Update is called once per frame
         void Update()
