7f5646c6dcbac3e61c7d45d68170c45e7aa3fb8d
RENNES\b.carion <baptiste.carion@sfr.fr>
2022-10-31 15:52:39 +0100

    Commit before pull to solve bug in input manager

--- Assets/Baptiste/Scripts/PolishCowGifScript.cs
+++ Assets/Baptiste/Scripts/PolishCowGifScript.cs
@@ -1,37 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Baptiste;
 
-public class PolishCowGifScript : MonoBehaviour
+namespace Baptiste
 {
- 
-    public float Delay = 0.1f;
-    public Material[] Materials;
-    public Renderer TargetRenderer;
-    private int _currentIndex = 0;
-    private float _elapsedTime = 0;
-
-    // Start is called before the first frame update
-    void Start()
+    public class PolishCowGifScript : MonoBehaviour
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        _elapsedTime += Time.deltaTime;
-        if (_elapsedTime <= Delay)
+        public float Delay = 0.1f;
+        public Material[] Materials;
+        public Renderer TargetRenderer;
+        private int _currentIndex = 0;
+        private float _elapsedTime = 0;
+
+        // Start is called before the first frame update
+        void Start()
         {
-            return;
+
         }
-        _elapsedTime = 0;
-        _currentIndex++;
-        if (_currentIndex >= Materials.Length)
+
+        // Update is called once per frame
+        void Update()
         {
-            _currentIndex = 0;
+            _elapsedTime += Time.deltaTime;
+            if (_elapsedTime <= Delay)
+            {
+                return;
+            }
+            _elapsedTime = 0;
+            _currentIndex++;
+            if (_currentIndex >= Materials.Length)
+            {
+                _currentIndex = 0;
+            }
+            TargetRenderer.material = Materials[_currentIndex];
         }
-        TargetRenderer.material = Materials[_currentIndex];
     }
 }
 
