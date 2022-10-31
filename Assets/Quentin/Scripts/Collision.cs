27d330aa2260f4cde1de6208aabc99bab459883e
RENNES\q.izadpanah <q.izadpanah@rns.ecolescreatives.com>
2022-10-31 16:59:53 +0100

    Hitbox

--- Assets/Quentin/Scripts/Collision.cs
+++ Assets/Quentin/Scripts/Collision.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletManager : MonoBehaviour
+public class Collision : MonoBehaviour
+    
 {
+    public PolygonCollider2D contact;
+    public bool gameOver;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameOver = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if ( gameOver == true)
+        {
+            Debug.Log("Yo");
+        }
     }
 }
