a0650b705de1bbf75ca2020b01b0d03a8cedb587
RENNES\s.osborne <s.osborne98@outlook.fr>
2022-11-17 09:02:25 +0100

    Sauvegarde_5

--- Assets/Sebastien/Scripts/BalloonCursor.cs
+++ Assets/Sebastien/Scripts/BalloonCursor.cs
@@ -4,68 +4,33 @@ using UnityEngine;
 
 namespace Osborne
 {
-    public class BalloonCursor : MonoBehaviour
+
+public class BalloonCursor : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
     {
-        // Update is called once per frame
-       /* void Update()
+        if (Input.anyKeyDown)
         {
-            if (mainCamera.WorldToViewportPoint(transform.position).x > 1)
+            var rectTransform = (transform as RectTransform);
+            var positionTemporaire = new Vector3(
+                rectTransform.anchoredPosition.x / Camera.main.pixelWidth,
+                rectTransform.anchoredPosition.y / Camera.main.pixelHeight * -1,
+                Camera.main.nearClipPlane);
+            var convertedPosition = Camera.main.ViewportToWorldPoint(positionTemporaire);
+
+            var positionDansLeMonde = new Vector3(convertedPosition.x, -convertedPosition.y, convertedPosition.z);
+
+            if (Physics.Raycast(positionDansLeMonde, Camera.main.transform.forward, out var info))
             {
-                var tempVectorZero = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, mainCamera.farClipPlane));
-
-                transform.position = new Vector3(tempVectorZero.x, transform.position.y, transform.position.z);
+                var ball = info.transform.GetComponent<BalloonCollector>();
+                if (ball != null)
+                {
+                    //Debug.Log(ball.alacon);
+                }
             }
-            if (mainCamera.WorldToViewportPoint(transform.position).x < 0)
-            {
-                var tempVectorOne = mainCamera.ViewportToWorldPoint(new Vector3(1, 1, mainCamera.farClipPlane));
-
-                transform.position = new Vector3(tempVectorOne.x, transform.position.y, transform.position.z);
-            }
-
-            if (mainCamera.WorldToViewportPoint(transform.position).y > 1)
-            {
-                var tempVectorZero = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, mainCamera.farClipPlane));
-
-                transform.position = new Vector3(transform.position.x, tempVectorZero.y, transform.position.z);
-            }
-            if (mainCamera.WorldToViewportPoint(transform.position).y < 0)
-            {
-                var tempVectorOne = mainCamera.ViewportToWorldPoint(new Vector3(1, 1, mainCamera.farClipPlane));
-
-                transform.position = new Vector3(transform.position.x, tempVectorOne.y, transform.position.z);
-            }
-
+          }
         }
-
-        public void MoveUp()
-        {
-            subject.position += subject.up * speed;
-        }
-        public void MoveDown()
-        {
-            subject.position -= subject.up * speed;
-        }
-        public void MoveRight()
-        {
-            subject.position += subject.right * speed;
-        }
-        public void MoveLeft()
-        {
-            subject.position -= subject.right * speed;
-        }
-        public void Action()
-        {
-
-            if (listfenetre.isGood == true)
-            {
-                Debug.Log("Victoire");
-            }
-            else
-            {
-                Debug.Log("Perdu");
-            }
-
-        }*/
-
     }
+
 }
