0bd4b2fb770cbebf1f9f7a56373cc2160ff38b10
RENNES\q.izadpanah <q.izadpanah@rns.ecolescreatives.com>
2022-11-17 11:31:25 +0100

    Collider

--- Assets/Louis/Scripts/InputManager.cs
+++ Assets/Louis/Scripts/InputManager.cs
@@ -9,7 +9,7 @@ public class InputManager : MonoBehaviour
     public InputStruct4 directions;
     public InputStruct2 pointsMorts;
     public InputStruct3  action;
-    private float deadZone = 0.4f;
+    private float deadZone = 0.1f;
     private bool isPointMortH = true, isPointMortV = true;
     private void Update()
     {
--- Assets/Quentin/Scripts/Collision.cs
+++ Assets/Quentin/Scripts/Collision.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Collision : MonoBehaviour
     
 {
-    public PolygonCollider2D contact;
     public bool gameOver;
     // Start is called before the first frame update
     void Start()
@@ -16,9 +15,16 @@ public class Collision : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if ( gameOver == true)
         {
             Debug.Log("Yo");
         }
     }
+    void OnTriggerEnter(Collider other)
+    {
+        gameOver = true;
+        Destroy(this);
+    }
+    
 }
