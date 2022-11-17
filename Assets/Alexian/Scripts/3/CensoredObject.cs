a9dfc41732096908a135a1c164cd31ca20b6ccec
a.raguin <a.raguin@rns.ecolescreatives.com>
2022-11-17 11:33:12 +0100

    Mini-Jeu3 Advance

--- Assets/Alexian/Scripts/3/CensoredObject.cs
+++ Assets/Alexian/Scripts/3/CensoredObject.cs
@@ -5,17 +5,35 @@ using UnityEngine;
 public class CensoredObject : MonoBehaviour
 {
     public Transform positionObject;
+    public RectTransform sizeBlur;
     public bool isMoving;
     public bool firstClick;
+    public RectTransform rtImage;
     // Start is called before the first frame update
     void Start()
     {
-        
+        RandomBlur();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void RandomBlur()
+    {
+        var widthImage = rtImage.rect.width;
+        var heightImage = rtImage.rect.height;
+
+        var widthCensor = Random.Range(0, widthImage);
+        var heightCensor = Random.Range(0, heightImage);
+
+        sizeBlur.sizeDelta = new Vector2(widthCensor, heightCensor);
+
+        Debug.Log(widthImage);
+        Debug.Log(widthCensor);
+        Debug.Log(heightImage);
+        Debug.Log(heightCensor);
     }
 }
