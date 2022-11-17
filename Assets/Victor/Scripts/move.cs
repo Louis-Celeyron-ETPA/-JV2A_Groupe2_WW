0a6eda1ed2e00d3b3c919b280a285588f2187ad2
RENNES\v.fabre <v.fabre@rns.ecolescreatives.com>
2022-11-17 15:16:46 +0100

    debut  jeu 2

---
+++ Assets/Victor/Scripts/move.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+
+    public Transform subject;
+    public float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Physics.Raycast(subject.position, Vector3.down,1.1f)) 
+        {
+
+            speed = 0;
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void MoveRight()
+    {
+        subject.position += subject.right * speed;
+        if (Physics.Raycast(subject.position, Vector3.down, 4f))
+        {
+
+            speed = 0;
+
+        }
+    }
+    public void MoveLeft()
+    {
+        subject.position -= subject.right * speed;
+        if (Physics.Raycast(subject.position, Vector3.down, 4f))
+        {
+
+            speed = 0;
+
+        }
+    }
+
+
+
+}
