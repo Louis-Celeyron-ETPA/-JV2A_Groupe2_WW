4bca36a6897817ca1e344eb90fae375cc0be9f4d
RENNES\s.osborne <s.osborne98@outlook.fr>
2022-11-28 14:25:15 +0100

    Sauvegarde_9

--- Assets/Sebastien/Scripts/BalloonMouvement.cs
+++ Assets/Sebastien/Scripts/BalloonMouvement.cs
@@ -4,18 +4,36 @@ using UnityEngine;
 
 namespace Osborne
 {
-
     public class BalloonMouvement : MonoBehaviour
     {
         [SerializeField] float speed = 1f;
 
-    // Update is called once per frame
+        public Transform subject;
+
+        // Update is called once per frame
         void Update()
         {
-            transform.position += new Vector3(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed, 0);
+           // transform.position += new Vector3(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed, 0);
+        }
+
+        public void MoveUp()
+        {
+            subject.position += subject.up * speed;
+        }
+        public void MoveDown()
+        {
+            subject.position -= subject.up * speed;
+        }
+        public void MoveRight()
+        {
+            subject.position += subject.right * speed;
+        }
+        public void MoveLeft()
+        {
+            subject.position -= subject.right * speed;
         }
 
 
-    }
+        }
 
 }
