58f942d1bb3639dadc06bfd5860e8affcdaf5553
RENNES\v.fabre <v.fabre@rns.ecolescreatives.com>
2023-01-05 11:00:05 +0100

    jeu trois fini faire un hud et refaire le jeu 1

--- Assets/Victor/Scripts/scriptFeur.cs
+++ Assets/Victor/Scripts/scriptFeur.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class scriptFeur : MonoBehaviour
 {
+    public MeshRenderer mr1;
+    public float timerFeur;
+    public float timerReset;
+    public bool isGood = false;
+    public bool isFini = false;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        timerFeur = Random.Range(2f, 4f);
+
+        mr1.material.color = new Color(1.0f, 0, 0);
+
+
+        StartCoroutine(routine(timerFeur, timerReset)) ;
+
     }
 
     // Update is called once per frame
@@ -17,8 +30,36 @@ public class scriptFeur : MonoBehaviour
     {
         
     }
+
+    public IEnumerator routine(float timerFeur, float timerReset)
+    {
+       
+        yield return new WaitForSeconds(timerFeur);
+        mr1.material.color = new Color(0,1.0f,0);
+
+        isGood = true;
+
+        yield return new WaitForSeconds( timerFeur + timerReset);
+        mr1.material.color = new Color(1.0f, 0, 0);
+
+        isGood = false;
+
+    }
+
     public void Action()
     {
-     
+
+        if (isGood == true && isFini == false)
+        {
+            Debug.Log("victoire");
+            isFini = true;
+        }
+
+        if (isGood == false && isFini == false)
+        {
+            Debug.Log("defaite");
+            isFini = true;
+        }
+
     }
 }
