212222fdd85e452ec58b37958d6740b3f673787d
RENNES\s.osborne <s.osborne98@outlook.fr>
2023-01-23 16:37:41 +0100

    Sauvegarde_Terminer_Seb

---
+++ Assets/Sebastien/Scripts/HexagonTimer.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace osborne
+{
+
+    public class HexagonTimer : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        public float timeRemaining = 20;
+        public bool timeIsRunning = false;
+        public TextMeshProUGUI timeText;
+
+
+        private void Start()
+        {
+            timeIsRunning = true;
+        }
+
+        void Update()
+        {
+            if (timeIsRunning == true)
+            {
+                if (timeRemaining > 0)
+                {
+                    timeRemaining -= Time.deltaTime;
+                }
+            }
+            DisplayTime(timeRemaining);
+        }
+
+
+        void DisplayTime(float timeToDisplay)
+        {
+            //timeToDisplay += 1;
+            float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+            timeText.text = string.Format("{0:00}", seconds);
+
+        }
+
+    }
+
+}
