e2e56dc016c1b2698550589c1b60a2d3feb7d0b8
RENNES\l.lucas <lancelotlucasecole@gmail.com>
2023-01-23 17:05:12 +0100

    modification sans résultat...

--- Assets/Louis/Scripts/UpdateScore.cs
+++ Assets/Louis/Scripts/UpdateScore.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class Player : MonoBehaviour
+public class UpdateScore : MonoBehaviour
 {
+
+    public TextMeshProUGUI score;
     // Start is called before the first frame update
     void Start()
     {
-        
+        score.text = ManagerManager.GlobalGameManager.MinigamesFinished.ToString("00");
     }
 
     // Update is called once per frame
